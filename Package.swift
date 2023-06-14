7305c24934ad25956eaadd2d88bfecad3511d5a6
Christian Treffs <christian.treffs@deepup.de>
2023-06-14 08:46:00 +0200

    Cleanup

--- Package.swift
+++ Package.swift
@@ -1,12 +1,9 @@
-// swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.5
 import PackageDescription
 
 let package = Package(
     name: "SwiftJXL",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftJXL",
             targets: [
@@ -14,19 +11,11 @@ let package = Package(
             ]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftJXL",
             dependencies: [
-                //.target(name: "CJXL")
                 .target(name: "jxl")
             ]),
-        //.systemLibrary(name: "jxl", path: "/usr/local/lib")
-        /*.target(
-            name: "CJXL",
-            linkerSettings: [.linkedLibrary("libjxl")]
-        ),*/
         .binaryTarget(name: "jxl", path: "jxl.xcframework"),
 
     ]
